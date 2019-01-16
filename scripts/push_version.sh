292213fb12e1adfcfce60080f1ea35a10c8c5881
Thomas Maurin <maur1th@users.noreply.github.com>
2019-01-17 00:00:17 +0100

    Bump Cargo.toml version on tag

---
+++ scripts/push_version.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -ex
+
+if [ -n "$TRAVIS_TAG" ];
+then
+    sed -i "s/^version \{0,1\}=.*/version = \"${TRAVIS_TAG:1}\"/" Cargo.toml
+    git config --local user.name "Travis CI"
+    git config --local user.email "travis@travis-ci.org"
+    git add Cargo.toml
+    git commit --allow-empty -m "Bump version to $TRAVIS_TAG [skip ci]"
+    git push https://${GITHUB_TOKEN}@github.com/maur1th/task_diff.git HEAD:master
+fi
